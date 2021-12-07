5ee79b3e3e6dad80fda908ef11115dfb4e1e7278
Mark Lisanti <lisantimark@gmail.com>
2021-12-07 01:25:29 -0700

    fix

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 // Basic express/node server to test websocket connections
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const WebSocket = require("ws");
+const http = require('http');
+const WebSocket = require('ws');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", (ws) => {
+wss.on('connection', (ws) => {
     // log that a client connected 
     console.log('new client connected');
 
-    ws.on("message", (message) => {
+    ws.on('message', (message) => {
         console.log('recieved: ' + message.toString());
 
         // for now just send out to all clients
@@ -23,11 +23,11 @@ wss.on("connection", (ws) => {
     });
 });
 
-app.get("/", (req, res) => {
-  res.send("Running WebSockets");
+app.get('/', (req, res) => {
+  res.send('Running WebSockets');
 });
 
 server.listen(8082, () => {
-  console.log("Listening to port 8082");
+  console.log('Listening to port 8082');
 });
 
